ecc4b63aa8347034b9df66da1501f9cf65703882
Srikanth Ellendula <srikanthellendula48@gmail.com>
2024-04-14 22:08:06 +0530

    third commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -19,12 +19,14 @@ const Home = () => {
         <div className='products-list-container'>
              {data.map(eachItem => 
             <div key={eachItem.id} className='products-list-container-div'> 
-                 <img className='products-list-container-img' src={eachItem.image} alt={eachItem.title}/>                
+                <img className='products-list-container-img' src={eachItem.image} alt={eachItem.title}/>                
                 <h4> {eachItem.title.substring(0, 30)+'..'}</h4>                
+                
                 <div className='add-cont'> 
                 <p> <span className='new-price'> &#8377;{Math.ceil(eachItem.price * 80)}</span> <span className='old-price'> {parseInt(eachItem.price)*160} </span> <span className='offer'> (50% off) </span>   </p>
                 <img className='add-icon' alt='add' src='https://res.cloudinary.com/dx94215tz/image/upload/v1713066746/Group_450_kpi1ay.png'/>
                 </div>
+
             </div>
             
             )}
